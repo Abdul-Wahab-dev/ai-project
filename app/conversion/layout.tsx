838a460ddf7d33c1129a857fa3d843b2ab739e0a
abdulwahab <abdulwahabdev0@gmail.com>
2024-03-04 01:37:35 +0500

    adding content for tools

--- app/conversion/layout.tsx
+++ app/conversion/layout.tsx
@@ -23,6 +23,135 @@ function Layout({ children }: { children: React.ReactNode }) {
             <div className=" py-10 px-3 overflow-hidden">{children}</div>
           </div>
         </div>
+        <div className="bg-white w-full h-full py-10 px-3">
+          <div className="custom_container mx-auto h-full px-3 py-10 text-black">
+            <div className="pb-5 border-b">
+              <h2 className="text-2xl font-semibold uppercase">
+                You can transform image format by following steps:
+              </h2>
+              <ul>
+                <li>
+                  <p className="text-lg">1. Upload your image.</p>
+                </li>
+                <li>
+                  <p className="text-lg">2. Select the Format.</p>
+                </li>
+                <li>
+                  <p className="text-lg">3. Download the image.</p>
+                </li>
+              </ul>
+            </div>
+
+            {/* Advantanges */}
+            <div className="w-full h-full flex flex-col items-center py-5 gap-5">
+              <div className="my-5">
+                <h2 className="text-2xl font-semibold uppercase text-center">
+                  Transform Image Format Online - Free and Convenient
+                </h2>
+                <p className="uppercase text-lg text-center">
+                  Transform the image Format Without Compromising Quality in
+                  Just a Few Clicks
+                </p>
+              </div>
+
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 md:gap-x-14 xl:gap-x-20 gap-y-10 items-start">
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Quick Guide to Transform image format
+                  </h3>
+                  <p className="text-justify">
+                    Uploading your file is simple and convenient, offering you
+                    two easy methods to get started: Simply drag and drop or
+                    browse your hard drive
+                  </p>
+                  <p className="text-justify">
+                    Once you uploaded the image, then set the format you wanted.
+                  </p>
+                  <p className="text-justify">
+                    Click the "Convert image" button to begin the Transformation
+                    process. Once complete, you'll be able to preview the
+                    trasnformed format image and download it to your computer
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Why Do We Need To Transform Image Format?
+                  </h3>
+                  <p className="text-justify">
+                    Different devices and softwares support different image
+                    formats. Transformed images can be viewed and manipulated
+                    across various platforms without compatibility issues.
+                  </p>
+                  <p className="text-justify">
+                    Some efficient image formats preserve their quality while
+                    sharing. Certain formats support specific features. For
+                    example, PNG supports transparency, while JPEG support image
+                    compression.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Maintained Image Quality
+                  </h3>
+                  <p className="text-justify">
+                    Toolefy maintains your image Quality while transforming it.
+                    Color sharpness, and Transperacy are the factors which
+                    determines the image Quality.
+                  </p>
+                  <p className="text-justify">
+                    Toolefy maintaines these Factors while transforming your
+                    image.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Secure Image Compression
+                  </h3>
+                  <p className="text-justify">
+                    Security is an important concern while handling sensitive
+                    content.Feel free to use this tool. No privacy concerns
+                    associated with your images.
+                  </p>
+                  <p className="text-justify">
+                    Toolefy prioritizes the visitor's privacy by not storing it
+                    in its database.Toolefy image transform tool regulates the
+                    laws of data protection.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Cost and Time - Saving
+                  </h3>
+                  <p className="text-justify">
+                    You can enjoy your desired image Format by just a few
+                    clicks.This Tool saves your Time as well as cost.
+                  </p>
+                  <p className="text-justify">
+                    You don't have to go through complicated processes to get
+                    your desired format. Just need to upload your image on
+                    Toolefy and Enjoy the results.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Device-Friendly Tool
+                  </h3>
+                  <p className="text-justify">
+                    This tool comforts you to use it from any device either a
+                    mobile, PC or laptop. You just need a device with stable
+                    internet connection to compress your image.
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* Advantanges */}
+          </div>
+        </div>
         <MoreTools url="conversion" />
       </div>
     </div>
--- app/image-to-pdf/layout.tsx
+++ app/image-to-pdf/layout.tsx
@@ -20,6 +20,132 @@ function Layout({ children }: { children: React.ReactNode }) {
             <div className=" py-10 px-3 overflow-hidden">{children}</div>
           </div>
         </div>
+        <div className="bg-white w-full h-full py-10 px-3">
+          <div className="custom_container mx-auto h-full px-3 py-10 text-black">
+            <div className="pb-5 border-b">
+              <h2 className="text-2xl font-semibold uppercase">
+                You can transform image format by following steps:
+              </h2>
+              <ul>
+                <li>
+                  <p className="text-lg">1. Upload your images.</p>
+                </li>
+                <li>
+                  <p className="text-lg">2. Convert to PDF.</p>
+                </li>
+                <li>
+                  <p className="text-lg">3. Download the file.</p>
+                </li>
+              </ul>
+            </div>
+
+            {/* Advantanges */}
+            <div className="w-full h-full flex flex-col items-center py-5 gap-5">
+              <div className="my-5">
+                <h2 className="text-2xl font-semibold uppercase text-center">
+                  Change IMAGEs inTO PDF online - Free and Convenient
+                </h2>
+                <p className="uppercase text-lg text-center">
+                  Convert images into PDF with Just a Few Clicks
+                </p>
+              </div>
+
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 md:gap-x-14 xl:gap-x-20 gap-y-10 items-start">
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Quick Guide to Transform image format
+                  </h3>
+                  <p className="text-justify">
+                    Uploading your file is simple and convenient, offering you
+                    two easy methods to get started: Simply drag and drop or
+                    browse your hard drive
+                  </p>
+                  <p className="text-justify">
+                    Once you uploaded the images, Click the "Convert PDF" button
+                    to begin the Transformation process
+                  </p>
+                  <p className="text-justify">
+                    Once complete, you'll be able to download it to your
+                    computer
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Why Do We Convert Images to PDF?
+                  </h3>
+                  <p className="text-justify">
+                    PDF is more sustainable in its quality and layout. PDFs are
+                    less suspected of Errors as compared to image formats.
+                  </p>
+                  <p className="text-justify">
+                    PDF is widely used to share documents.PDFs are easier to
+                    store, share, messaging and uploading.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Maintained Quality
+                  </h3>
+                  <p className="text-justify">
+                    PDF is the format that retains its quality and layout. PDFs
+                    are generally more stable and less prone to alterations as
+                    compared to image formats like JPEG or PNG
+                  </p>
+                  <p className="text-justify">
+                    PDF ensures consistency across different devices and
+                    platforms.Our Image to PDF Tool maintains the same layout
+                    and appearance regardless of the viewing device or software
+                    used.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Security
+                  </h3>
+                  <p className="text-justify">
+                    Security is an important concern while handling sensitive
+                    content.Feel free to use this tool. No privacy concerns
+                    associated with your files.
+                  </p>
+                  <p className="text-justify">
+                    Toolefy prioritizes the visitor's privacy by not storing it
+                    in its database.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Cost and Time - Saving
+                  </h3>
+                  <p className="text-justify">
+                    You can get the result by just a few clicks.This Tool saves
+                    your Time as well as cost.
+                  </p>
+                  <p className="text-justify">
+                    You don't have to go through complicated processes to get
+                    your desired result. Just need to upload your file on
+                    Toolefy and Enjoy the results.
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-end gap-2">
+                  <h3 className="text-xl font-semibold text-center">
+                    Device-Friendly Tool
+                  </h3>
+                  <p className="text-justify">
+                    This tool comforts you to use it from any device either a
+                    mobile, PC or laptop. You just need a device with stable
+                    internet connection.
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* Advantanges */}
+          </div>
+        </div>
         <MoreTools url="image-to-pdf" />
       </div>
     </div>
